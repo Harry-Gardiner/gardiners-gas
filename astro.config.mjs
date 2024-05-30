b64b727ee5f7d9a2d3871f7dde7325054e23040d
Harry Gardiner Lunar <harry@lunar.build>
2024-05-30 12:26:54 +0100

    Swapped all images into image_assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // // https://astro.build/config
 export default defineConfig({
-    site: 'https://Harry-Gardiner.github.io',
+    site: 'https://harry-gardiner.github.io',
 });
