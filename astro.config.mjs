2abee9f3527f7fb2265f305ba8276b59282189ae
Harry Gardiner Lunar <harry@lunar.build>
2024-05-30 08:56:41 +0100

    Cap site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // // https://astro.build/config
 export default defineConfig({
-    site: 'https://harry-gardiner.github.io',
+    site: 'https://Harry-Gardiner.github.io',
 });
